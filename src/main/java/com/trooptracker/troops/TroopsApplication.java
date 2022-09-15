66555b49fb6a580b74ce3be63f015b1bf2575005
Benjiman Sutton <benjimansutton@gmail.com>
2022-09-15 13:49:39 +0100

    first commit

---
+++ src/main/java/com/trooptracker/troops/TroopsApplication.java
@@ -0,0 +1,13 @@
+package com.trooptracker.troops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trooptracker/troops/TroopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trooptracker.troops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
